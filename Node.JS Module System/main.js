3f5a8fe21e87e3245b59ec6ae795d805a7380a3c
Samuel Meza <71412765+sammeza10@users.noreply.github.com>
2023-06-28 14:26:46 -0600

    Add files via upload

---
+++ Node.JS Module System/main.js
@@ -0,0 +1,8 @@
+// main.js
+const { sum, sub } = require("./operation.js");
+
+let num1 = 4;
+let num2 = 9;
+
+console.log(sum(num1, num2));
+console.log(sub(num1, num2));
---
+++ Node.JS Module System/operation.js
@@ -0,0 +1,13 @@
+// operations.js
+function sum(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  sum,
+  sub,
+};
